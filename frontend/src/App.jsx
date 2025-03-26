3ee2da25d3372e036700d77649743901cac30f78
AnujYelve <anujyelve3074@gmail.com>
2025-03-26 16:28:47 +0530

    Dr_alka component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav1 from './components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from "./components/mid-image/mid-image";
+import Dr_Alka from "./components/Dr.Alka/Dr.Alka";
 import Footer from "./components/footer/Footer"
 import NotificationsSection from "./components/Notifications/notice"
 
@@ -12,6 +13,7 @@ const App = () => {
     <div>
       <Nav1/>
       < Image/>
+      <Dr_Alka/>
       <NotificationsSection/>
       <Footer/>
       
