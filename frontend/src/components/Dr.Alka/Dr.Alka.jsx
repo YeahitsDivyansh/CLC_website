b519c2672ee1d3935ad6d9725c5f4cb0dd2541bb
AnujYelve <anujyelve3074@gmail.com>
2025-03-26 17:08:46 +0530

    Dr_Alka section styling

--- frontend/src/components/Dr.Alka/Dr.Alka.jsx
+++ frontend/src/components/Dr.Alka/Dr.Alka.jsx
@@ -2,18 +2,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function WelcomeSection() {
   return (
-    <div className="container mb-5 py-4">
+    <div className="container py-4">
       {/* Heading Section */}
       <div className="text-center mb-4">
         <h2 className="text-primary">Welcome to Campus Law Centre, Faculty of Law, University of Delhi</h2>
       </div>
 
       {/* Dr. Alka Chawla Section */}
-      <div className="position-relative mt-4 d-flex justify-content-center">
-        <div className="card shadow-sm border mx-auto" style={{ maxWidth: '600px', borderRadius: '10px', borderColor: '#dee2e6' }}>
+      <div 
+      style={{
+        marginTop:"4rem"
+      }}
+      className="position-relative  d-flex justify-content-center">
+        <div
+        className="card shadow-sm mx-auto" style={{ maxWidth: '70rem', border:"3px outset rgba(181, 185, 194, 0.63)", borderRadius: '1rem', borderColor: '#dee2e6' }}>
           <div className="card-body pt-5">
             <div className="d-flex flex-column align-items-center">
-              <h5 className="text-primary fw-bold">Prof. (Dr.) Alka Chawla, Professor-In-Charge</h5>
+              <h5
+               className="text-primary fw-bold">Prof. (Dr.) Alka Chawla, Professor-In-Charge</h5>
               <p className="text-muted text-center">
                 Welcome to <strong>Campus Law Centre, Faculty of Law</strong>, Ours is a motivated community committed to intellectual inquiry and I am pleased to introduce you to it. I invite you to check out more of our website and other portals to discover us.
               </p>
@@ -21,10 +27,10 @@ export default function WelcomeSection() {
           </div>
         </div>
         <img
-          src="/path/to/image.jpg"
+          src="/src/assets/Dr_Alka-img.jpeg"
           alt="Prof. (Dr.) Alka Chawla"
           className="rounded-circle position-absolute bg-white"
-          style={{ width: '100px', height: '100px', objectFit: 'cover', border: '3px solid #eee', top: '0', transform: 'translateY(-50%)' }}
+          style={{ width: '100px', height: '100px', objectFit: 'cover', border: '2px ridge #a1a5b1a1', top: '0', transform: 'translateY(-50%)' }}
         />
       </div>
     </div>
