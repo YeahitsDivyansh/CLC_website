32bb0d3378e1232044c3f86296e30cd0741ce2f4
AnujYelve <anujyelve3074@gmail.com>
2025-03-27 16:12:03 +0530

    Navbar update and latest notice section added

---
+++ frontend/src/components/Latest_Notice/Latest_Notice.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const LatestNotices = () => {
+  return (
+    <div className="container mt-3">
+      <h4 className="fw-bold text-uppercase text-center border-bottom pb-2">
+        Latest Notices & Announcements
+      </h4>
+      <div className="bg-light p-2 mt-2">
+        <marquee behavior="scroll" direction="left" scrollamount="5">
+          <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
+            DATE SHEET FOR LL.B. II/IV/VI TERM EXAMINATIONS, JULY-AUGUST 2024
+          </a>
+          <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
+            ADMISSIONS OPEN FOR NEW SESSION 2024-25
+          </a>
+          <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
+            IMPORTANT NOTICE REGARDING EXAM FORM SUBMISSION
+          </a>
+        </marquee>
+      </div>
+    </div>
+  );
+};
+
+export default LatestNotices;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Footer from "../components/footer/Footer"
 import NotificationsSection from "../components/Notifications/notice"
 import Nav1 from '../components/Navbar/Navbar'
 import Dr_Alka from "../components/Dr.Alka/Dr.Alka";
+import Latest_Notice from "../components/Latest_Notice/Latest_Notice";
 
 const Home = () => {
   return (
     <div>
         <Nav1/>
+        <Latest_Notice/>
       <Image/>
       <Dr_Alka/>
       <NotificationsSection/>
