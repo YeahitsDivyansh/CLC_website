dcca23834767bf6d3bcd9bca8d216b2b7190976e
prakharujn1 <128589011+prakharujn1@users.noreply.github.com>
2025-04-04 18:05:30 +0530

    Update Latest_Notice.jsx

--- frontend/src/components/Latest_Notice/Latest_Notice.jsx
+++ frontend/src/components/Latest_Notice/Latest_Notice.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { server } from "../../main";
 
 const LatestNotices = () => {
+  const [notices, setNotices] = useState([]);
+
+  useEffect(() => {
+    const fetchNotices = async () => {
+      try {
+        const { data } = await axios.get(`${server}/api/latest-notices?populate=*`);
+        setNotices(data.data); // Corrected key
+        console.log(notices);
+      } catch (error) {
+        console.error("Error fetching notices:", error);
+      }
+    };
+
+    fetchNotices();
+  }, []);
+
+
   return (
     <div className="container mt-4">
       <h4 className="fw-bold text-uppercase text-center border-bottom pb-2">
@@ -9,7 +28,24 @@ const LatestNotices = () => {
       </h4>
       <div className="bg-light p-2 mt-2">
         <marquee behavior="scroll" direction="left" scrollamount="5">
-          <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
+
+          {
+            notices.map((e) => (
+
+              <a
+                href={e.Notice.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary fw-bold text-decoration-none mx-3"
+              >
+                {e.Title}
+              </a>
+
+            ))
+          }
+
+
+          {/* <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
             DATE SHEET FOR LL.B. II/IV/VI TERM EXAMINATIONS, JULY-AUGUST 2024
           </a>
           <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
@@ -17,7 +53,7 @@ const LatestNotices = () => {
           </a>
           <a href="#" className="text-primary fw-bold text-decoration-none mx-3">
             IMPORTANT NOTICE REGARDING EXAM FORM SUBMISSION
-          </a>
+          </a> */}
         </marquee>
       </div>
     </div>
