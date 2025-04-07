9784dd7efde81d898e9e422e0b67ef2727f8dc02
YeahitsDivyansh <divyansh972003@gmail.com>
2025-04-07 23:05:26 +0530

    Update Services_for_Students.jsx to enhance layout and add service links

--- frontend/src/DropdownRoutes/Forms/Services_for_Students.jsx
+++ frontend/src/DropdownRoutes/Forms/Services_for_Students.jsx
@@ -1,7 +1,170 @@
 import React from "react";
 
 const Services_for_Students = () => {
-  return <div>Services_for_Students</div>;
+  return (
+    <div className="bg-white">
+      <div
+        style={{
+          margin: "2.5rem 0rem",
+          color: "#4e459a",
+          fontSize: "30px",
+          fontFamily: "IBM Plex Sans",
+          fontWeight: "900",
+        }}
+        className="rounded-xl mt-5 mx-auto text-center items-center justify-center bg-gray-100 w-1/2 py-3"
+      >
+        <p className="px-5 font-semibold">Services for Students</p>
+      </div>
+
+      <div
+        style={{
+          margin: "2.5rem 0rem",
+          color: "#4e459a",
+          fontSize: "30px",
+          fontFamily: "IBM Plex Sans",
+          fontWeight: "900",
+        }}
+        className="rounded-xl mt-5 mx-auto text-center items-center justify-center bg-gray-100 w-1/2 py-3"
+      >
+        <p className="px-5 font-semibold">Related to Degree</p>
+      </div>
+
+      <div className="p-6 w-full max-w-2xl mx-auto">
+        <ul>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://exam.du.ac.in/?Services/Attestation-of-Degree"
+              className="text-blue-600 font-medium"
+            >
+              Attestation of Degree
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://exam.du.ac.in/?Services/Confidential-Result"
+              className="text-blue-600 font-medium"
+            >
+              Confidential Result
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://exam.du.ac.in/?Services/Degree-in-Advance"
+              className="text-blue-600 font-medium"
+            >
+              Degree in Advance
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://exam.du.ac.in/?Services/Duplicate-Degree-Diploma-Certificate"
+              className="text-blue-600 font-medium"
+            >
+              Duplicate Degree / Diploma / Certificate
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://exam.du.ac.in/?Services/Provisional-Certificate"
+              className="text-blue-600 font-medium"
+            >
+              Provisional Certificate
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://exam.du.ac.in/?Services/Request-for-Degree-Diploma-Certificate"
+              className="text-blue-600 font-medium"
+            >
+              Request for Degree / Diploma / Certificate
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://exam.du.ac.in/?Services/Request-for-Transcript"
+              className="text-blue-600 font-medium"
+            >
+              Request for Transcript
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://exam.du.ac.in/?Services/Request-for-Special-Certificate"
+              className="text-blue-600 font-medium"
+            >
+              Request for Special Certificate
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200 border-b border-gray-300">
+            <a
+              href="https://exam.du.ac.in/?Services/Verification-of-Degree"
+              className="text-blue-600 font-medium"
+            >
+              Verification of Degree
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div
+        style={{
+          margin: "2.5rem 0rem",
+          color: "#4e459a",
+          fontSize: "30px",
+          fontFamily: "IBM Plex Sans",
+          fontWeight: "900",
+        }}
+        className="rounded-xl mt-5 mx-auto text-center items-center justify-center bg-gray-100 w-1/2 py-3"
+      >
+        <p className="px-5 font-semibold">Related to Marksheet</p>
+      </div>
+
+      <div className="p-6 w-full max-w-2xl mx-auto">
+        <ul>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://durslt.du.ac.in/AC_INTERNET_Marksheet_NDB/Students/Combine_GradeCard.aspx"
+              className="text-blue-600 font-medium"
+            >
+              Statement of Marks / Score Card / Result
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://app.uod.ac.in/attestation/"
+              className="text-blue-600 font-medium"
+            >
+              Attestation of Marksheet
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://exam.du.ac.in/?Services/Duplicate-Marksheet"
+              className="text-blue-600 font-medium"
+            >
+              Duplicate Mark Sheet
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="https://app.uod.ac.in/migration/"
+              className="text-blue-600 font-medium"
+            >
+              Migration Certificate (Online)
+            </a>
+          </li>
+          <li className="p-3 bg-gray-200">
+            <a
+              href="https://www.du.ac.in/uploads/Forms/students_related_forms/19213_Migration.pdf"
+              className="text-blue-600 font-medium"
+            >
+              Migration Certificate (Offline Form)
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Services_for_Students;
